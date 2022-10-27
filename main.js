fd7ca3b56e12ea14996e5b8d02c9803aaeb0f54c
Egzavyer <xavier.lermusieaux@gmail.com>
2022-10-27 18:18:08 -0400

    added javascript to make reviews appear when scrolling

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
 //Year to Date calculator
-document.getElementById("yearToDate").innerHTML = "© YouTrient " + new Date().getFullYear();
+document.getElementById("yearToDate").innerHTML =
+  "© YouTrient " + new Date().getFullYear();
+
+//Reviews fade-in
+const faders = document.querySelectorAll(".fade-in");
+const appearOptions = {
+  threshold: 1,
+};
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
